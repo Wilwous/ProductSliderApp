c13d106e925f8a919f51796c1b8f9234c5c6e54b
Wilwous <mick.thomson.tm@gmail.com>
2024-08-09 17:18:30 +0300

    Initial Commit

---
+++ ProductSliderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductSliderApp
+//
+//  Created by Антон Павлов on 09.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
